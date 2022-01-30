9df7b0acd2483ef8fbd187c4f163cf26d3675b0a
Heimir Kristmundsson <heimir.97rgmail.com>
2022-01-30 19:04:02 +0100

    Tried two versions of login without success

--- BankWebApp/Controllers/LoginController.cs
+++ BankWebApp/Controllers/LoginController.cs
@@ -1,6 +1,106 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿//using BankWebApp.Models;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.IdentityModel.Tokens;
+//using System.IdentityModel.Tokens.Jwt;
+//using System.Security.Claims;
+//using System.Text;
+
+//namespace BankWebApp.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class LoginController : ControllerBase
+//    {
+//        private IConfiguration _config;
+//        private readonly RoleManager<IdentityRole> _roleManager;
+//        private readonly UserManager<User> _userManager;
+//        private readonly SignInManager<User> _signInManager;
+//        private readonly BankContext _context;
+
+//        public LoginController(IConfiguration config, UserManager<User> userManager, SignInManager<User> signInManager, BankContext context)
+//        {
+//            _config = config;
+//            _userManager = userManager;
+//            _signInManager = signInManager;
+//            _context = context;
+//        }
+
+//        [AllowAnonymous]
+//        [HttpPost]
+//        public IActionResult Login([FromBody] User userLogin)
+//        {
+//             Task<User> user = Authenticate(userLogin);
+
+//            if (user != null)
+//            {
+//                var token = Generate(user);
+//                return Ok(token);
+//            }
+
+//            return NotFound("User not found");
+//        }
+
+
+//        private async Task<string> Generate(User user)
+//        {
+//            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+//            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+//            //var role = await _userManager.GetRolesAsync(user);
+
+//            var claims = new[]
+//           {
+//                new Claim(ClaimTypes.NameIdentifier, user.UserName),
+//                new Claim(ClaimTypes.Email, user.Email),
+//                //new Claim(ClaimTypes.GivenName, user.Customer.Givenname),
+//                //new Claim(ClaimTypes.Surname, user.Customer.),
+//                //new Claim(ClaimTypes.Role, role)
+//            };
+
+//            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+//              _config["Jwt:Audience"],
+//              claims,
+//              expires: DateTime.Now.AddMinutes(15),
+//              signingCredentials: credentials);
+
+//            return new JwtSecurityTokenHandler().WriteToken(token);
+//        }
+
+//        private async Task<User> Authenticate(User userLogin)
+//        {
+
+//            var result = await _signInManager.PasswordSignInAsync(userLogin.Email,
+//                            userLogin.PasswordHash, false, lockoutOnFailure: true);
+
+
+//            if (result.Succeeded)
+//            {
+//                var currentUser = userLogin;
+//                return currentUser;
+//            }
+
+//            return null;
+//        }
+//    }
+//}
+
+
+
+//NEXT TRY NOT USING IDENTITY TO LOGIN
+
+
+using BankWebApp.MobileApiModels;
+using BankWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace BankWebApp.Controllers
 {
@@ -9,28 +109,78 @@ namespace BankWebApp.Controllers
     public class LoginController : ControllerBase
     {
         private IConfiguration _config;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly BankContext _context;
 
-        public LoginController(IConfiguration config)
+        public LoginController(IConfiguration config, UserManager<User> userManager, SignInManager<User> signInManager, BankContext context)
         {
             _config = config;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
         }
 
         [AllowAnonymous]
         [HttpPost]
-        //public IActionResult Login([FromBody] string UserName, string Password)
-        //{
-        //    var user = Authenticate(userLogin);
+        public IActionResult Login([FromBody] UserLoginModel userLogin)
+        {
+            var user = Authenticate(userLogin);
 
-        //    if (user != null)
-        //    {
-        //        var token = Generate(user);
-        //        return Ok(token);
-        //    }
+            if (user != null)
+            {
+                var token = Generate(user);
+                return Ok(token);
+            }
 
-        //    return NotFound("User not found");
-        //}
+            return NotFound("User not found");
+        }
+
+        [AllowAnonymous]
+        [HttpGet("test")]
+        public IActionResult Test([FromBody] User userLogin)
+        {
+            return Ok("Successfull test");
+        }
 
 
-        
+        private string Generate(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            //var role = await _userManager.GetRolesAsync(user);
+
+            var claims = new[]
+           {
+                new Claim(ClaimTypes.NameIdentifier, user.UserName),
+                new Claim(ClaimTypes.Email, user.Email),
+                //new Claim(ClaimTypes.GivenName, user.Customer.Givenname),
+                //new Claim(ClaimTypes.Surname, user.Customer.),
+                //new Claim(ClaimTypes.Role, role)
+            };
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Audience"],
+              claims,
+              expires: DateTime.Now.AddMinutes(15),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private User Authenticate(UserLoginModel userLogin)
+        {
+
+            var currentUser = _context.Users.FirstOrDefault(o => o.UserName.ToLower() == userLogin.Username.ToLower() && o.PasswordHash == userLogin.Password);
+
+            if (currentUser != null)
+            {
+                return currentUser;
+            }
+
+            return null;
+        }
     }
 }
+
