cd1a8ad974f621158246a12038ccb6cf110f4de1
Heimir Kristmundsson <heimir.97rgmail.com>
2022-02-07 11:01:10 +0100

    Removed leftover from response cashing test

--- BankWebApp/Pages/Countries/Country.cshtml.cs
+++ BankWebApp/Pages/Countries/Country.cshtml.cs
@@ -19,14 +19,11 @@ namespace BankWebApp.Pages.Countries
         public string CountryFlag { get; set; }
         public string CountryName { get; set; }
 
-        public DateTime ResponseTest { get; set; }
-
         public void OnGet(string countryName)
         {
             TopCustomers = _countryService.GetCountryData(countryName);
             CountryFlag = BankWebApp.Data.Countries.CountryFlags[countryName];
             CountryName = countryName;
-            ResponseTest = DateTime.Now;
         }
     }
 }
